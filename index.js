5600a7ed07734ad654339da105d140b1cb507fb9
joaopedromata <joaopmata182@gmail.com>
2020-05-12 20:12:08 -0300

    back-end finish

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const handlebars = require('express-handlebars')
+const path = require('path')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//Config
+    // Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    const viewsPath = path.join(__dirname, '/views')
+    app.use('/', express.static(viewsPath));
+
+
+app.get('/', (req, res) => {
+    const axios = require('axios')
+    axios.get('https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,currencies&key=762f83be').then((response) => {
+    const USD = response.data.currencies.USD.buy
+    res.render('index', {USD: USD})
+    })
+})
+
+
+app.listen(3000)
+
+
+/*
+const axios = require('axios')
+
+axios.get('https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,currencies&key=762f83be').then((response) => {
+    
+    var dolar = response.data.currencies.USD.buy
+    var mostrardolar = ''
+    console.log(dolar)
+})
+*/
+
+
+
+
+
+
+
